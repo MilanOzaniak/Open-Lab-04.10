7dbf44a8429d38b84f55a839c8ed38c2d4b9a719
Ozaniak <Ozaniak@PC>
2020-10-23 01:43:31 +0200

    hotovo

--- Open-Lab-04.10/Calculator.cs
+++ Open-Lab-04.10/Calculator.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
+
 namespace Open_Lab_04._10
 {
     public class Calculator
     {
         public float Average(int[] nums)
         {
-            throw new NotImplementedException();
+            float sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            return sum / nums.Length;
+            
+
         }
     }
 }
